5787bcf9dd6eb146b0699814d3232bfe011ea966
majorz <zahari@resin.io>
2018-08-28 00:13:45 +0300

    Client::get_active_connections

--- src/client.rs
+++ src/client.rs
@@ -2,6 +2,7 @@ use glib::translate::*;
 
 use ffi;
 
+use ActiveConnection;
 use Client;
 use Device;
 
@@ -10,5 +11,9 @@ use array::gptrarray_to_vec;
 impl Client {
     pub fn get_devices(&self) -> Vec<Device> {
         unsafe { gptrarray_to_vec(ffi::nm_client_get_devices(self.to_glib_none().0)) }
+    }
+
+    pub fn get_active_connections(&self) -> Vec<ActiveConnection> {
+        unsafe { gptrarray_to_vec(ffi::nm_client_get_active_connections(self.to_glib_none().0)) }
     }
 }
