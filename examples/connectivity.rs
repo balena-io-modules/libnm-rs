6892b472e64267314ac877b8110eade6ba65908c
majorz <zahari@resin.io>
2018-08-28 11:10:24 +0300

    Use get_property

--- examples/connectivity.rs
+++ examples/connectivity.rs
@@ -1,7 +1,19 @@
 extern crate nm;
 
+use nm::prelude::*;
+
 fn main() {
     let client = nm::Client::new(None).unwrap();
     let connectivity = client.get_connectivity();
     println!("Connectivity: {:?}", connectivity);
+
+    let connectivity_check_enabled: bool = client
+        .get_property("connectivity-check-enabled")
+        .unwrap()
+        .get()
+        .unwrap();
+    println!(
+        "Connectivity Check Enabled: {:?}",
+        connectivity_check_enabled
+    );
 }
