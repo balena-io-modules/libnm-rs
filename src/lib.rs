e8722e9569f413460d8048e7ce67210a0f2baad2
Zahari Petkov <zahari@balena.io>
2020-05-28 11:33:56 +0300

    Allow clippy::too_many_arguments
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::cast_ptr_alignment))]
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::should_implement_trait))]
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::missing_safety_doc))]
+#![cfg_attr(feature = "cargo-clippy", allow(clippy::too_many_arguments))]
 
 #[macro_use]
 extern crate bitflags;
