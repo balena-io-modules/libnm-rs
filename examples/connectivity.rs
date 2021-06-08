c77d2dff1a3f2c158fc434ce1dc87a29a4b88216
Zahari Petkov <zahari@balena.io>
2021-06-08 17:58:21 +0300

    Make connectivity example async
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- examples/connectivity.rs
+++ examples/connectivity.rs
@@ -2,11 +2,27 @@ extern crate nm;
 
 use nm::*;
 
-fn main() {
-    let client = Client::new(NONE_CANCELLABLE).unwrap();
-    let connectivity = client.connectivity();
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    let context = glib::MainContext::default();
+
+    context.block_on(run())
+}
+
+async fn run() -> Result<()> {
+    let client = Client::new_async_future()
+        .await
+        .context("Failed to create NM Client")?;
+
+    let connectivity = client
+        .check_connectivity_async_future()
+        .await
+        .context("Failed to check connectivity")?;
     let check_enabled = client.connectivity_check_get_enabled();
 
     println!("Connectivity: {:?}", connectivity);
     println!("Connectivity check enabled: {:?}", check_enabled);
+
+    Ok(())
 }
